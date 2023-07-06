7546134a98a667fa66274906394b5a9ab1d8b11e
jyang624 <128828917+jyang624@users.noreply.github.com>
2023-07-05 23:22:56 -0400

    Delete spotify_game directory

--- spotify_game/run.py
+++
@@ -1,7 +0,0 @@
-from app.py import get_user_playlist,
-
-username = input("Enter your Spotify username: ")
-playlist_name = input("Enter the name of the playlist from which you want to play a random track: ")
-track_name = input("Enter the name of the random track to play: ")
-
-play_track(track_name)
